a268d6b963131d82a66c3edadf687a02ca73faa0
wheatup <acphun@gmail.com>
2020-08-04 17:26:24 +0900

    supports string dictionary

--- hexie.js
+++ hexie.js
@@ -1,7 +1,7 @@
 const _dict = ['富强', '民主', '文明', '和谐', '自由', '平等', '公正', '法治', '爱国', '敬业', '诚信', '友善'];
 
 const encode = (src, dict = _dict) => {
-	dict = [...new Set(dict)];
+	dict = [...new Set(Array.isArray(dict) ? dict : [...dict])];
 	const codes = [...src].map(e => e.charCodeAt(0).toString(dict.length - 1)).join(',');
 	const map = Object.fromEntries([
 		...dict.map((e, i, arr) => [i.toString(arr.length - 1), e]).slice(0, dict.length - 1),
@@ -12,7 +12,7 @@ const encode = (src, dict = _dict) => {
 }
 
 const decode = (src, dict = _dict) => {
-	dict = [...new Set(dict)];
+	dict = [...new Set(Array.isArray(dict) ? dict : [...dict])];
 	const raw = dict.reduce((acc, key, index) =>
 		acc.replace(new RegExp(key, 'gm'), index === dict.length - 1 ? ',' : `{{${index.toString(dict.length - 1)}}}`), src);
 	return raw.split(',').map(e => String.fromCharCode(parseInt(e.replace(/{{(.)}}/g, (_, i) => i), dict.length - 1))).join('');
