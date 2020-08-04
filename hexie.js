31690d43bb3da8c02fa5473f30a1620da9ea9ee0
wheatup <acphun@gmail.com>
2020-08-04 18:42:03 +0900

    supports dictionary size greater than 36

--- hexie.js
+++ hexie.js
@@ -2,20 +2,34 @@ const _dict = ['富强', '民主', '文明', '和谐', '自由', '平等', '公�
 
 const encode = (src, dict = _dict) => {
 	dict = [...new Set(Array.isArray(dict) ? dict : [...dict])];
-	const codes = [...src].map(e => e.charCodeAt(0).toString(dict.length - 1)).join(',');
-	const map = Object.fromEntries([
-		...dict.map((e, i, arr) => [i.toString(arr.length - 1), e]).slice(0, dict.length - 1),
-		[',', dict[dict.length - 1]]
-	]);
-	const result = [...codes].map(c => map[c]).join('');
+	const decimal = dict.length - 1;
+	const codes = [...src].map(e => {
+		const code = e.charCodeAt(0);
+
+		const digits = [code % decimal];
+		let remainder = code;
+		while ((remainder /= decimal) > 1) {
+			digits.unshift(remainder % decimal ^ 0);
+		}
+		return digits;
+	});
+
+	const result = [...codes].map((c, i) => c.map(d => dict[d]).join('') + (i < codes.length - 1 ? dict[dict.length - 1] : '')).join('');
 	return result;
 }
 
 const decode = (src, dict = _dict) => {
 	dict = [...new Set(Array.isArray(dict) ? dict : [...dict])];
 	const raw = dict.reduce((acc, key, index) =>
-		acc.replace(new RegExp(key, 'gm'), index === dict.length - 1 ? ',' : `{{${index.toString(dict.length - 1)}}}`), src);
-	return raw.split(',').map(e => String.fromCharCode(parseInt(e.replace(/{{(.)}}/g, (_, i) => i), dict.length - 1))).join('');
+		acc.replace(new RegExp(key, 'gm'), index === dict.length - 1 ? ',' : `{${index}}`), src);
+
+	const result = raw.split(',')
+		.map(e => /{(.+?)}/g[Symbol.match](e)
+			.map(e => +e.replace(/[\{\}]/g, ''))
+			.reduce((acc, cur, i, arr) => acc + cur * ((dict.length - 1) ** (arr.length - i - 1)), 0)
+		).map(e => String.fromCharCode(e)).join('');
+
+	return result
 }
 
 if (typeof module !== 'undefined') {
